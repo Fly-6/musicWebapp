fb26d3fd763ed8e2696b0f88ac583ddcec59b612
范国康 <782216385@qq.com>
2017-03-05 11:44:39 +0800

    heard创建

---
+++ src/components/searchHead.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <div class="input-b">
+            <i class="icon"></i>
+            <div>
+                <i class="icon"></i>
+                <input type="search" placeholder="输入歌名、歌星、歌词等">
+            </div>
+            <span>取消</span>
+            <i class="icon"></i>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
